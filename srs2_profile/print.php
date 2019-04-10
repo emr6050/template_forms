a2f260813d0f0bb1b4f20a505a339ea3d808a9ca
DebSch <47372659+DebSch@users.noreply.github.com>
2019-04-09 21:37:59 -0500

    Revised print file

--- srs2_profile/print.php
+++ srs2_profile/print.php
@@ -62,56 +62,74 @@ Printed on <?php echo date("F d, Y", time()); ?>
 <form method=post action="">
 <span class="title"><?php xl($form_name, 'e'); ?></span><br>
 
-<!-- container for the main body of the form -->
-<div id="print_form_container">
+<div id="form_container">
+  <div id="general">
+    <h3>Form Information</h3>
+    Assessment Id: <input type='text' size='25' name='assessment_id' id='assessment_id' value='<?php echo stripslashes($record['assessment_id']);?>'/>
+    <br><br>
+    Assessment Age:
+          <input type="radio" name="assessment_age" value="preschool" <?php if ($record["assessment_age"] == 'preschool') { echo "CHECKED"; } ?>>Preschool (ages 2 1/2 to 4 1/2) &nbsp;
+          <input type="radio" name="assessment_age" value="school_age" <?php if ($record["assessment_age"] == 'school_age') { echo "CHECKED"; } ?>/>School-Age (ages 4 to 18) &nbsp; 
+          <input type="radio" name="assessment_age" value="adult" <?php if ($record["assessment_age"] == 'adult') { echo "CHECKED"; } ?>/>Adult (ages 19+)
+    <br><br>
+    Rater Name: <input type='text' size='50' name='rater_name' id='rater_name' value='<?php echo stripslashes($record['rater_name']);?>'/>
+    <br><br>
+    Rater Relationship:
+          <input type="radio" name="rater_relationship" value="mother" <?php if ($record["rater_relationship"] == 'mother') { echo "CHECKED"; } ?>/>Mother &nbsp;
+          <input type="radio" name="rater_relationship" value="father" <?php if ($record["rater_relationship"] == 'father') { echo "CHECKED"; } ?>/>Father &nbsp;
+          <input type="radio" name="rater_relationship" value="other_custodial_adult" <?php if ($record["rater_relationship"] == 'other_custodial_adult') { echo "CHECKED"; } ?>/>Other Custodial Adult &nbsp;
+          <input type="radio" name="rater_relationship" value="teacher" <?php if ($record["rater_relationship"] == 'teacher') { echo "CHECKED"; } ?>/>Teacher &nbsp;
+          <input type="radio" name="rater_relationship" value="other_specialist" <?php if ($record["rater_relationship"] == 'other_specialist') { echo "CHECKED"; } ?>/>Other Specialist
+    <br><br>
+    Facility: <input type='text' size='50' name='facility' id='facility' value='<?php echo stripslashes($record['facility']);?>'/>
+    <br><br>
+    Date:
+      <input type='text' size='10' class='datepicker' name='form_date' id='form_date' value='<?php echo stripslashes($record['form_date']);?>' title='<?php xl('yyyy-mm-dd', 'e'); ?>' />
+  </div>
+  
+  <div id="scores">
+    <h4>SRS-2 Total Score Results</h4>
+    SRS-2 Total Raw score: <input type='text' name='srs2_total_raw_score' id='srs2_total_raw_score' value='<?php echo stripslashes($record['srs2_total_raw_score']);?>'/>
+    <br><br>
+    SRS-2 T-score: <input type='text' name='srs2_t_score' id='srs2_t_score' value='<?php echo stripslashes($record['srs2_t_score']);?>'/>
+    <br><br>
+    <h4>DSM-5 Compatible Scales</h4>
+    DSM-5 SCI Raw score: <input type='text' name='dsm5_sci_raw_score' id='dsm5_sci_raw_score' value='<?php echo stripslashes($record['dsm5_sci_raw_score']);?>'/>
+    <br><br>
+    DSM-5 SCI T-score: <input type='text' name='dsm5_sci_t_score' id='dsm5_sci_t_score' value='<?php echo stripslashes($record['dsm5_sci_t_score']);?>'/>
+    <br><br>
+    DSM-5 RRB Raw score: <input type='text' name='dsm5_rrb_raw_score' id='dsm5_rrb_raw_score' value='<?php echo stripslashes($record['dsm5_rrb_raw_score']);?>'/>
+    <br><br>
+    DSM-5 RRB T-score: <input type='text' name='dsm5_rrb_t_score' id='dsm5_rrb_t_score' value='<?php echo stripslashes($record['dsm5_rrb_t_score']);?>'/>
+    <br><br>
+    <h4>Treatment Subscales</h4>
+    Awr Raw score: <input type='text' name='subscale_awr_raw_score' id='subscale_awr_raw_score' value='<?php echo stripslashes($record['subscale_awr_raw_score']);?>'/>
+    <br><br>
+    Awr T-score: <input type='text' name='subscale_awr_t_score' id='subscale_awr_t_score' value='<?php echo stripslashes($record['subscale_awr_t_score']);?>'/>
+    <br><br>
+    Cog Raw score: <input type='text' name='subscale_cog_raw_score' id='subscale_cog_raw_score' value='<?php echo stripslashes($record['subscale_cog_raw_score']);?>'/>
+    <br><br>
+    Cog T-score: <input type='text' name='subscale_cog_t_score' id='subscale_cog_t_score' value='<?php echo stripslashes($record['subscale_cog_t_score']);?>'/>
+    <br><br>
+    Com Raw score: <input type='text' name='subscale_com_raw_score' id='subscale_com_raw_score' value='<?php echo stripslashes($record['subscale_com_raw_score']);?>'/>
+    <br><br>
+    Com T-score: <input type='text' name='subscale_com_t_score' id='subscale_com_t_score' value='<?php echo stripslashes($record['subscale_com_t_score']);?>'/>
+    <br><br>
+    Mot Raw score: <input type='text' name='subscale_mot_raw_score' id='subscale_mot_raw_score' value='<?php echo stripslashes($record['subscale_mot_raw_score']);?>'/>
+    <br><br>
+    Mot T-score: <input type='text' name='subscale_mot_t_score' id='subscale_mot_t_score' value='<?php echo stripslashes($record['subscale_mot_t_score']);?>'/>
+    <br><br>
+    RRB Raw score: <input type='text' name='subscale_rrb_raw_score' id='subscale_rrb_raw_score' value='<?php echo stripslashes($record['subscale_rrb_raw_score']);?>'/>
+    <br><br>
+    RRB T-score: <input type='text' name='subscale_rrb_t_score' id='subscale_rrb_t_score' value='<?php echo stripslashes($record['subscale_rrb_t_score']);?>'/>
+    <br><br>
+  </div>
 
-<div id="print_general">
-<table>
-<tr><td>
-Date:
-   <input type='text' size='10' name='form_date' id='form_date'
-    value='<?php echo stripslashes($record['form_date']);?>'
-    title='<?php xl('yyyy-mm-dd', 'e'); ?>'
-    />
-</td></tr>
-<tr><td>
-Name: <input id="name" name="name" type="text" size="50" maxlength="250" value="<?php echo stripslashes($record['name']);?>">
-Date of Birth:
-   <input type='text' size='10' name='dob' id='dob'
-    value='<?php echo stripslashes($record['dob']);?>'
-    title='<?php xl('yyyy-mm-dd Date of Birth', 'e'); ?>'
-    />
-</td></tr>
-<tr><td>
-Phone: <input name="phone" id="phone" type="text" size="15" maxlength="15" value="<?php echo stripslashes($record['phone']);?>">
-</td></tr>
-<tr><td>
-Address: <input name="address" id="address" type="text" size="80" maxlength="250" value="<?php echo stripslashes($record['address']);?>">
-</td></tr>
-</table>
-</div>
-
-<div id="print_bottom">
-Use this space to express notes <br>
-<textarea name="notes" id="notes" cols="80" rows="4"><?php echo stripslashes($record['notes']);?></textarea>
-<br><br>
-<div style="text-align:right;">
-Signature? 
-<input type="radio" id="sig" name="sig" value="y" <?php if ($record["sig"] == 'y') {
-    echo "CHECKED";
-} ?>>Yes
-/
-<input type="radio" id="sig" name="sig" value="n" <?php if ($record["sig"] == 'n') {
-    echo "CHECKED";
-} ?>>No
-&nbsp;&nbsp;
-Date of signature: 
-   <input type='text' size='10' name='sig_date' id='sig_date'
-    value='<?php echo stripslashes($record['sig_date']);?>'
-    title='<?php xl('yyyy-mm-dd', 'e'); ?>' />
-</div>
-</div>
-
+  <div id="extra">
+    <h4>Notes</h4>
+    <textarea name="notes" id="notes" cols="80" rows="4"><?php echo stripslashes($record['notes']);?></textarea>
+    <br><br>
+  </div>
 </div> <!-- end form_container -->
 
 </form>
